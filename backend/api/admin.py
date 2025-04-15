a08ff69c54769c407c3c649f6bf7d79c4367bbd8
md ehtesham <ehteshammd089@gmail.com>
2025-04-15 13:02:52 +0530

    FullMedicineDetails

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from . views import Company, Medicine, AdminLogin, Customer, Bill, BillDetails
+
+from django.contrib import admin
+admin.site.register(Company)
+admin.site.register(Medicine)
+admin.site.register(AdminLogin)
+admin.site.register(Customer)
+admin.site.register(Bill)
+admin.site.register(BillDetails)
